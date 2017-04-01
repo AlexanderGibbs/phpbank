e4efa27cb054007428a2bd2c7a15a1988a18dfcc
AlexanderGibbs <primeminister@stravinia.com>
2017-04-01 13:00:12 -0500

    v1.0 upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// The data required can usually be found on your host's website, or in the registration email you got from your host.
+$datpass="<replace this>"; //mysql password
+$dathost="<replace this>"; //databse server 
+$datusr="<replace this>"; //user name
+$datname="<replace this>"; //normally the same as above
+?>
